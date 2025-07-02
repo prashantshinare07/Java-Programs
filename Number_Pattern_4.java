528a2d556464feb7fccc182f963860bc091d9d41
Prashant Shinare <prashantshinare11@gmail.com>
2025-07-02 23:18:12 +0530

    Add files via upload

---
+++ Number_Pattern_4.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class Number_Pattern_4 {
+
+	public static void main(String[] args)
+	{
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=i; j>=1; j--)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
